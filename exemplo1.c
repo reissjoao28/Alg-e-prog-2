184f7507cc1fe5d93a977d444ac44eaf0403fea1
Reiss <reissjoao28@gmail.com>
2025-08-12 21:50:51 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,14 @@
+/* Lab .1
+  * Exemplo 1 */
+
+#include <stdio.h>
+
+int main(){
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf("Prog. II\n");
+	printf("Turma 02N");
+	
+	return 0;
+	
+}
